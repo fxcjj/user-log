77b937262b5a2455a7b0cf81a9e78e0ca1922705
lihua.luo <lihua.luo@picahealth.com>
2017-11-11 19:58:34 +0800

    create user-log

---
+++ src/main/java/com/vic/log/App.java
@@ -0,0 +1,14 @@
+package com.vic.log;
+
+import com.vic.core.User;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		User user = new User();
+	}
+}
